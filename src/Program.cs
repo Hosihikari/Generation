01e3a434647a33de5c53e9d94a29cf31e0a30310
予纾 <starsdream00@icloud.com>
2023-12-29 14:19:14 +0800

    feat: Add a watcher for the generating

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,9 @@
-﻿using Hosihikari.Generation;
+﻿using System.Diagnostics;
+using Hosihikari.Generation;
 
+Stopwatch watcher = new();
+Console.WriteLine($"Start generating at {DateTime.Now}");
+watcher.Start();
 Main.Run(new("originalData.json", "out"));
+watcher.Stop();
+Console.WriteLine($"Generated successfully at {DateTime.Now}, which took {watcher.Elapsed}");
