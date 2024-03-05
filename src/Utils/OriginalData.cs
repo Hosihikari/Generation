3a1b15cad614900e748d433349552c83904667cb
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-03-05 12:34:15 +0800

    fix: Fix classes of original data

--- src/Utils/OriginalData.cs
+++ src/Utils/OriginalData.cs
@@ -2,63 +2,68 @@ using System.Text.Json.Serialization;
 
 namespace Hosihikari.Generation.Utils;
 
-public struct OriginalData
-{
-    [JsonPropertyName("classes")] public Dictionary<string, Class> Classes { get; set; }
-    [JsonPropertyName("fn_list")] public Dictionary<string, int> FunctionList { get; set; }
-    [JsonPropertyName("identifier")] public IdentifierData Identifier { get; set; }
-    [JsonPropertyName("sha_256_hash")] public Hashs Sha256Hash { get; set; }
-}
+public record OriginalData(
+    [property: JsonPropertyName("classes")]
+    Dictionary<string, Class> Classes,
+    [property: JsonPropertyName("identifier")]
+    IdentifierData Identifier,
+    [property: JsonPropertyName("sha_256_hash")]
+    Hashs Sha256Hash);
 
-public struct Class
-{
-    [JsonPropertyName("parent_types")] public List<string> ParentTypes { get; set; }
-    [JsonPropertyName("vtbl_entry")] public List<string> VtblEntry { get; set; }
-    [JsonPropertyName("child_types")] public List<string> ChildTypes { get; set; }
-    [JsonPropertyName("public")] public List<Item> Public { get; set; }
-    [JsonPropertyName("protected")] public List<Item> Protected { get; set; }
-    [JsonPropertyName("private")] public List<Item> Private { get; set; }
-    [JsonPropertyName("public.static")] public List<Item> PublicStatic { get; set; }
-    [JsonPropertyName("private.static")] public List<Item> PrivateStatic { get; set; }
-    [JsonPropertyName("virtual")] public List<Item> Virtual { get; set; }
+public record Class(
+    [property: JsonPropertyName("parent_types")]
+    string[]? ParentTypes,
+    [property: JsonPropertyName("vtbl_entry")]
+    string[]? VtblEntry,
+    [property: JsonPropertyName("child_types")]
+    string[]? ChildTypes,
+    [property: JsonPropertyName("public")] Item[]? Public,
+    [property: JsonPropertyName("protected")]
+    Item[]? Protected,
+    [property: JsonPropertyName("private")]
+    Item[]? Private,
+    [property: JsonPropertyName("public.static")]
+    Item[]? PublicStatic,
+    [property: JsonPropertyName("protected.static")]
+    Item[]? ProtectedStatic,
+    [property: JsonPropertyName("private.static")]
+    Item[]? PrivateStatic,
+    [property: JsonPropertyName("virtual")]
+    Item[]? Virtual,
+    [property: JsonPropertyName("virtual.unordered")]
+    Item[]? VirtualUnordered);
 
-    [JsonPropertyName("virtual.unordered")]
-    public List<Item> VirtualUnordered { get; set; }
-}
+public record IdentifierData(
+    [property: JsonPropertyName("class")] string[] Class,
+    [property: JsonPropertyName("struct")] string[] Struct);
 
-public struct IdentifierData
-{
-    [JsonPropertyName("class")] public List<string> Class { get; set; }
-    [JsonPropertyName("struct")] public List<string> Struct { get; set; }
-}
+public record Hashs(
+    [property: JsonPropertyName("bedrock_server.exe")]
+    string Exe,
+    [property: JsonPropertyName("bedrock_server.pdb")]
+    string Pdb,
+    [property: JsonPropertyName("bedrock_server_symbols.debug")]
+    string Elf);
 
-public struct Hashs
-{
-    [JsonPropertyName("bedrock_server.exe")]
-    public string Exe { get; set; }
+public record Item(
+    [property: JsonPropertyName("access_type")]
+    int AccessType,
+    [property: JsonPropertyName("fake_symbol")]
+    string FakeSymbol,
+    [property: JsonPropertyName("flag_bits")]
+    int FlagBits,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("namespace")]
+    string Namespace,
+    [property: JsonPropertyName("params")] TypeData[]? Params,
+    [property: JsonPropertyName("rva")] ulong Rva,
+    [property: JsonPropertyName("storage_class")]
+    int StorageClass,
+    [property: JsonPropertyName("symbol")] string Symbol,
+    [property: JsonPropertyName("symbol_type")]
+    int SymbolType,
+    [property: JsonPropertyName("type")] TypeData Type);
 
-    [JsonPropertyName("bedrock_server.pdb")]
-    public string Pdb { get; set; }
-}
-
-public struct Item
-{
-    [JsonPropertyName("access_type")] public int AccessType { get; set; }
-    [JsonPropertyName("fake_symbol")] public string FakeSymbol { get; set; }
-    [JsonPropertyName("flag_bits")] public int FlagBits { get; set; }
-    [JsonPropertyName("name")] public string Name { get; set; }
-    [JsonPropertyName("namespace")] public string Namespace { get; set; }
-    [JsonPropertyName("params")] public List<TypeData>? Params { get; set; }
-    [JsonPropertyName("params_name")] public List<string>? ParamsName { get; set; }
-    [JsonPropertyName("rva")] public ulong Rva { get; set; }
-    [JsonPropertyName("storage_class")] public int StorageClass { get; set; }
-    [JsonPropertyName("symbol")] public string Symbol { get; set; }
-    [JsonPropertyName("symbol_type")] public int SymbolType { get; set; }
-    [JsonPropertyName("type")] public TypeData Type { get; set; }
-}
-
-public struct TypeData
-{
-    [JsonPropertyName("kind")] public int Kind { get; set; }
-    [JsonPropertyName("name")] public string Name { get; set; }
-}
+public record TypeData(
+    [property: JsonPropertyName("kind")] int Kind,
+    [property: JsonPropertyName("name")] string Name);
