c962baacdb6f3dd382da0d2155859e2c0bd34696
科幻大脑 <symelosky@yeah.net>
2024-03-13 20:24:51 +0800

    style: Change the McGenerator format

--- src/MinecraftExport/MCGenerator.cs
+++ src/MinecraftExport/MCGenerator.cs
@@ -3,51 +3,69 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Text.Json;
 
-namespace Hosihikari.Generation.MinecraftExport;
-
-internal sealed class McGenerator : IGenerator
+namespace Hosihikari.Generation.MinecraftExport
 {
-    private readonly AssemblyBuilder assemblyBuilder;
-    private readonly ModuleBuilder moduleBuilder;
-    private readonly OriginalData originalData;
-
-    public McGenerator(string originalFilePath)
+    internal sealed class McGenerator : IGenerator
     {
-        string originalDataJson = File.ReadAllText(originalFilePath);
-        originalData = JsonSerializer.Deserialize<OriginalData>(originalDataJson) ??
-                       throw new InvalidDataException("Incorrect original data file!");
-        AssemblyName assemblyName = new("Hosihikari.Minecraft");
-        assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess. /* Save */ Run);
-        moduleBuilder = assemblyBuilder.DefineDynamicModule("Hosihikari.Minecraft");
-    }
+        #region ---Private field---
 
-    public void Initialize()
-    {
-        using StreamWriter logFile = File.AppendText($"SkipedClass-{DateTime.Now:yyyy-M-dTHH-mm-ss}.log");
-        foreach ((string name, _) in originalData.Classes)
+        private readonly AssemblyBuilder assemblyBuilder;
+        private readonly ModuleBuilder moduleBuilder;
+        private readonly OriginalData originalData;
+
+        #endregion
+
+        #region ---Constructor---
+
+        public McGenerator(string originalFilePath)
         {
-            if (Shared.SkippedOnContain.Any(str => name.Contains(str)))
-            {
-                logFile.WriteLine(name);
-                continue;
-            }
-
-            moduleBuilder.DefineType(name);
+            string originalDataJson = File.ReadAllText(originalFilePath);
+            originalData = JsonSerializer.Deserialize<OriginalData>(originalDataJson) ??
+                           throw new InvalidDataException("Incorrect original data file!");
+            AssemblyName assemblyName = new("Hosihikari.Minecraft");
+            assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess. /* Save */ Run);
+            moduleBuilder = assemblyBuilder.DefineDynamicModule("Hosihikari.Minecraft");
         }
-    }
 
-    public void Run()
-    {
-        throw new NotImplementedException();
-    }
+        #endregion
 
-    public void Save(string path)
-    {
-        assemblyBuilder.Save(path);
-    }
+        #region ---Public method---
 
-    private void HandleMembers(Item[] member)
-    {
-        throw new NotImplementedException();
+        public void Initialize()
+        {
+            using StreamWriter logFile = File.AppendText($"SkipedClass-{DateTime.Now:yyyy-M-dTHH-mm-ss}.log");
+            foreach ((string name, _) in originalData.Classes)
+            {
+                if (Shared.SkippedOnContain.Any(str => name.Contains(str)))
+                {
+                    logFile.WriteLine(name);
+                    continue;
+                }
+
+                moduleBuilder.DefineType(name);
+            }
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string path)
+        {
+            assemblyBuilder.Save(path);
+        }
+
+        #endregion
+
+
+        #region ---Private method---
+
+        private void HandleMembers(Item[] member)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
